10e06e025838b2e9725063a557e76fb33d56873d
Marco Beyer <marco.beyer@fokus.fraunhofer.de>
2017-07-11 10:24:14 +0200

    bing news importer implemented

--- bingnews2json.py
+++ bingnews2json.py
@@ -1,6 +1,17 @@
 import requests as rq
 from pymongo import MongoClient
+import time
+import json
+import os
 
-url = 'https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=&mkt=de-de'
-headers = {'Authorization': apikey, 'Content-Type' : 'application/json'}
-r = rq.get(url, data=dumps(item), headers=headers)
+source = 'Microsoft'
+apikey = 'badb775479d94c869170ee6fc860433f'
+url = 'https://api.cognitive.microsoft.com/bing/v7.0/news?mkt=de-de'
+headers = {'Ocp-Apim-Subscription-Key': apikey, 'Content-Type' : 'application/json'}
+r = rq.get(url, headers=headers)
+imported_news = 'imported'
+if not os.path.isdir(imported_news):
+    os.mkdir(imported_news)
+filename = '%s_%s.json' % (source, time.time())
+with open(os.path.join(imported_news, filename), 'w+', encoding='utf8') as f:
+    json.dump(r.json(), f, ensure_ascii=False)
